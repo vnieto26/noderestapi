6dcbc4c1cc1076951f2b9763b7bcd5a71618545d
Victor Nieto <vnieto26@yahoo.com.mx>
2021-12-22 15:11:35 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://dbUser:9sZJwYhayVhc7tuO@cluster0.pwvr1.mongodb.net/dbproject?retryWrites=true&w=majority",
-  //MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/dbproject",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/dbproject",
   PORT: process.env.PORT || 5000,
   SECRET: 'projects-api'
 };
