a9ca0ce48d67c7b6bc168e0ead3fbe92e56f5a91
Victor Nieto <vnieto26@yahoo.com.mx>
2025-05-25 23:27:25 -0500

    Correccion conexion Mongodb

--- src/database.js
+++ src/database.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 import config from "./config";
 
 mongoose
-	.connect(config.MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+	.connect(config.MONGODB_URI)
 	.then((db) => console.log(`Base de datos conectada`))
 	.catch((err) => console.log(err));
